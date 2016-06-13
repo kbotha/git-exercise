894d6772a06567e2d0c9f72ffc53622f4ef2de74
kbotha <kristo.botha@gmail.com>
2016-06-13 13:53:21 +0200

    New chanage on Master
    change on master

--- src/test.py
+++ src/test.py
@@ -2,7 +2,7 @@ print "Calculating success rate..."
 print "Calculating diagnostics..."
 
 print "Success rate is too low!"
-print "Success rate: 0%."
+print "Success rate: 100%."
 
 print "rrr"
 print "It is broken!"
